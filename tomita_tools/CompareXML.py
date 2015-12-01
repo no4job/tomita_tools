ba289387b8173bf02af1dd962603091e19b12050
mdu <no_job@mail.ru>
2015-12-01 21:34:45 +0300

    сформирован список объектов с заменами

---
+++ tomita_tools/CompareXML.py
@@ -0,0 +1,101 @@
+#coding windows-1251
+__author__ = 'mdu'
+import io
+import re
+import operator
+from tomita_tools import Fragments
+from io import StringIO
+from datetime import datetime
+try:
+    from lxml import etree
+except ImportError:
+    print("lxml import error")
+    exit(1)
+class ParametersForCompare():
+    def __init__(self, **kwargs):
+        #absolute or relative path to input reference XML markup file
+        self.inputReferenceXMLMarkupFile = kwargs.get('INPUT_REFERENCE_XML_MARKUP_FILE','')
+        #absolute or relative path to input reference XML markup file encoding
+        self.inputReferenceXMLMarkupFileEncoding = kwargs.get('INPUT_REFERENCE_XML_MARKUP_FILE_ENCODING','')
+        #absolute or relative path to input compared XML markup file
+        self.inputComparedXMLMarkupFile = kwargs.get('INPUT_COMPARED_XML_MARKUP_FILE','cp1251')
+        #absolute or relative path to input compared XML markup file encoding
+        self.inputComparedXMLMarkupFileEncoding = kwargs.get('INPUT_COMPARED_XML_MARKUP_FILE_ENCODING','cp1251')
+        #absolute or relative path to output file with comparison results
+        self.outputComparisonResults = kwargs.get('OUTPUT_COMPARISON_RESULTS','cp1251')
+
+class difElement():
+    def __init__(self, **kwargs):
+        self.text = kwargs.get('TEXT','')
+        self.position = kwargs.get('POSITION',0)
+        self.length = kwargs.get('LENGTH',0)
+        self.source = kwargs.get('SOURCE','')
+#*********************************************************************************
+
+
+
+def compare(parameters: ParametersForCompare)-> ParametersForCompare:
+    s=""
+    #create list reference replacements
+    referenceReplacements = {}
+    referenceRoot = etree.parse(parameters.inputReferenceXMLMarkupFile)
+    referenceReplacementsElements= referenceRoot.xpath(".//facts/descendant-or-self::*[@pos]")
+
+    for replacement in referenceReplacementsElements:
+        replacementFragment = difElement()
+        replacementFragment.text = replacement.find("ReplacementText").attrib["val"]
+        replacementFragment.position = replacement.attrib["pos"]
+        replacementFragment.length = replacement.attrib["len"]
+        replacementFragment.source = "r"
+        key = str(replacementFragment.position).zfill(10)+";"+str(replacementFragment.length).zfill(10)
+        referenceReplacements[key]=replacementFragment
+
+
+    #create list compared replacements
+    comparedReplacements = {}
+    comparedRoot = etree.parse(parameters.inputComparedXMLMarkupFile)
+    comparedReplacementsElements= comparedRoot.xpath(".//facts/descendant-or-self::*[@pos]")
+    for replacement in comparedReplacementsElements:
+        replacementFragment = difElement()
+        replacementFragment.text = replacement.find("ReplacementText").attrib["val"]
+        replacementFragment.position = replacement.attrib["pos"]
+        replacementFragment.length = replacement.attrib["len"]
+        replacementFragment.source = "c"
+        key = str(replacementFragment.position).zfill(10)+";"+str(replacementFragment.length).zfill(10)
+        comparedReplacements[key]=replacementFragment
+
+    #create list of unmatched replacements
+    diffList={}
+    for key in referenceReplacements:
+        if key not in  comparedReplacements:
+            #keyParts = key.split(";")
+            diffList[key.split(";")[0]+"0"+key.split(";")[1]]=referenceReplacements[key]
+    for key in comparedReplacements:
+        if key not in  referenceReplacements:
+            diffList[key.split(";")[0]+"1"+key.split(";")[1]]=comparedReplacements[key]
+
+    s=""
+
+    diffListKeys= list(diffList.keys())
+    diffListKeys.sort()
+    for difListKey in   diffListKeys:
+        diff=diffList[difListKey]
+        if diff.source == "r":
+            source="Reference"
+        elif diff.source == "c":
+            source="Compared: "
+        else:
+            source="Uknown: "
+        s=s+source+"(pos= "+str(diff.position)+", len= "+str(diff.length)+"): "
+        s=s+diff.text+"\n"
+    with open(parameters.outputComparisonResults, "w") as f:
+        f.write(s)
+    return
+
+if __name__ == '__main__':
+    p =  ParametersForCompare()
+    p.inputReferenceXMLMarkupFile = "out_xml_ref.xml"
+    p.inputComparedXMLMarkupFile = "out_xml_cmp.xml"
+    p.outputComparisonResults = "compare.txt"
+    compare(p)
+    exit(0)
--- tomita_tools/compare_tool.py
+++ tomita_tools/compare_tool.py
@@ -1,35 +1,46 @@
 #coding windows-1251
 __author__ = 'mdu'
 import argparse
+from tomita_tools import MarkupXML
+from tomita_tools import CompareXML
 
-def markup(args):
-    output = "markuped file:{0}\noutput text file: {1}\noutput xml file: {2}"
-    output=output.format(args.markupedHTMLFile,args.outputTextFile,args.outputXMLFile)
-    print(output)
+def markupWithArgs (args):
+    #output = "markuped file:{0}\noutput text file: {1}\noutput xml file: {2}"
+    #output=output.format(args.markupedHTMLFile,args.outputTextFile,args.outputXMLFile)
+    #print(output)
+    p = MarkupXML.ParametersForMarkup()
+    p.inputMarkupedHTMLFile = args.markupedHTMLFile
+    p.outputTextFile = args.outputTextFile
+    p.outputXMLFile = args.outputXMLFile
+    MarkupXML.markup(p)
 
-def compare(args):
-    output = "xml file with reference replcacements:{0}\nxml file with parser replacements: {1}\noutput file: {2}"
-    output=output.format(args.referenceXMLFile,args.comparedXMLfile,args.comparisonOutputFile)
-    print(output)
-
+def compareWithArgs(args):
+    #output = "xml file with reference replcacements:{0}\nxml file with parser replacements: {1}\noutput file: {2}"
+    #output=output.format(args.referenceXMLFile,args.comparedXMLfile,args.comparisonOutputFile)
+    #print(output)
+    p = CompareXML.ParametersForCompare()
+    p.inputReferenceXMLMarkupFile = args.referenceXMLFile
+    p.inputComparedXMLMarkupFile = args.comparedXMLfile
+    p.outputComparisonResults = args.comparisonOutputFile
+    CompareXML.compare(p)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--version', action='version', version='1.0.0')
 subparsers = parser.add_subparsers()
 
-markup_parser = subparsers.add_parser('markup')
-markup_parser.add_argument('markupedHTMLFile', help='absolute or relative path to markuped html file')
+markup_parser = subparsers.add_parser('m')
+markup_parser.add_argument('markupedHTMLFile', help='absolute or relative path to input markuped html file')
 markup_parser.add_argument('outputTextFile', help='absolute or relative path to output text file')
 markup_parser.add_argument('outputXMLFile', help='absolute or relative path to output xml file')
-markup_parser.set_defaults(func=markup)
+markup_parser.set_defaults(func=markupWithArgs)
 
-compare_parser = subparsers.add_parser('compare')
+compare_parser = subparsers.add_parser('c')
 compare_parser.add_argument('referenceXMLFile', help='absolute or relative path to xml file with reference '
                                                      'replacements')
 compare_parser.add_argument('comparedXMLfile', help='absolute or relative path to compared xml file with replacements')
 compare_parser.add_argument('comparisonOutputFile', help='absolute or relative path to output file with comparison '
                                               'results')
-compare_parser.set_defaults(func=compare)
+compare_parser.set_defaults(func=compareWithArgs)
 
 if __name__ == '__main__':
     args = parser.parse_args()
