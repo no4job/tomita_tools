860e45ce6734777fce6254d45a2513a44f5ce882
no4job <no_job@mail.ru>
2016-05-01 20:26:25 +0300

    mongo db test

--- MongoTest/test.py
+++ MongoTest/test.py
@@ -1,4 +1,7 @@
+import copy
+
 __author__ = 'mdu'
+
 from pymongo import MongoClient
 from pymongo import errors
 import datetime
@@ -11,49 +14,8 @@ from datetime import datetime
 import sys
 sys.path.append("C:\IdeaProjects\tomita_tools\MongoTest")
 from Timer import *
+import json
 
-def get_model_element_(elem):
-    modelElement=[]
-#*****************************
-    modelElement.append(dict(parent_object=""))
-    modelElement.append(dict(revision=""))
-    modelElement.append(dict(model_revision=""))
-    modelElement.append(dict(description=""))
-    modelElement.append(dict(native_id=""))
-    modelElement.append(dict(creation_date=datetime.now()))
-    modelElement.append(dict(change_date=datetime.now()))
-    modelElement.append(dict(archive="false"))
-    modelElement.append(dict(deletede="false"))
-#***************************************************
-
-    modelElement.append(dict(name=next(iter(elem.xpath("./@ElementName")),"")))
-    modelElement.append(dict(id=next(iter(elem.xpath("./@ElementId")),"")))
-    modelElement.append(dict(type=next(iter(elem.xpath("./@TypeID")),"")))
-    data_section_2=[]
-    for attributeElement in elem.xpath("./www.qpr.com:Attribute",namespaces={'www.qpr.com': 'www.qpr.com'}):
-        fieldName=next(iter(attributeElement.xpath("./@AttributeName")),"")
-        field={}
-        field_values_array=[]
-        valueArray=[]
-        valueArray.extend(attributeElement.xpath("./www.qpr.com:Value/text()",namespaces={'www.qpr.com': 'www.qpr.com'}))
-        if len(valueArray):
-            field_values_array.append(dict(Value=valueArray))
-        records=[]
-        for recordElement in attributeElement.xpath("./www.qpr.com:Record",namespaces={'www.qpr.com': 'www.qpr.com'}):
-            record=[]
-            for recordFieldElement in recordElement.xpath("./www.qpr.com:Field",namespaces={'www.qpr.com': 'www.qpr.com'}):
-                recordFieldName=next(iter(recordFieldElement.xpath("./@Name")),"")
-                recordFieldValue=next(iter(recordFieldElement.xpath("./@Value")),"")
-                recordField={}
-                recordField[recordFieldName]=recordFieldValue
-                record.append(recordField)
-            records.append(record)
-        if len(records):
-            field_values_array.append(dict(Record=records))
-        field[fieldName]=field_values_array
-        data_section_2.append(field)
-    modelElement.append(dict(data_section_2=data_section_2))
-    return modelElement
 
 def get_model_element(elem):
     modelElement={}
@@ -74,6 +36,7 @@ def get_model_element(elem):
     data_section_2=[]
     for attributeElement in elem.xpath("./www.qpr.com:Attribute",namespaces={'www.qpr.com': 'www.qpr.com'}):
         fieldName=next(iter(attributeElement.xpath("./@AttributeName")),"")
+        fieldName=fieldName.replace('.', '\uff0E')
         field={}
         field_values_array=[]
         valueArray=[]
@@ -86,6 +49,7 @@ def get_model_element(elem):
             record={}
             for recordFieldElement in recordElement.xpath("./www.qpr.com:Field",namespaces={'www.qpr.com': 'www.qpr.com'}):
                 recordFieldName=next(iter(recordFieldElement.xpath("./@Name")),"")
+                recordFieldName=recordFieldName.replace('.', '\uff0E')
                 recordFieldValue=next(iter(recordFieldElement.xpath("./@Value")),"")
                 #recordField={}
                 #recordField[recordFieldName]=recordFieldValue
@@ -102,69 +66,96 @@ def uniq(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
+def remove_dots(data):
+    if type(data)is dict:
+        for key in data.keys():
+            if type(data[key]) is dict:
+                data[key] = remove_dots(data[key])
+            elif type(data[key])is list:
+                for item in data[key]:
+                    #remove_dots(item)
+                    if type(item)is dict or type(item)is list:
+                        remove_dots(item)
+
+            if '.' in key:
+                data[key.replace('.', '\uff0E')] = data[key]
+                #data[key.replace('.', '#')] = data[key]
+                del data[key]
+    if type(data)is list:
+        for item in data:
+            #remove_dots(item)
+            if type(item)is dict or type(item)is list:
+                remove_dots(item)
+        #if item is dict: data[key] = remove_dots(data[key])
+    return data
 if __name__ == '__main__':
+    time_total=Timer()
+    time_total.start()
     t = Timer()
     t.start()
     # start_time=datetime.now()
     # print("Start processing: "+start_time.strftime("%d.%m.%Y %H:%M:%S.%f"))
     client = MongoClient()
+    client.drop_database("modelDB")
     db=client.modelDB
+
     model = db.model
 
     #input_file='C:\\IdeaProjects\\hh_api_test\\MongoTest\\exp_types_formatted_few_elements.xml'
     #***input_file='exp_types_formatted_few_elements.xml'
-    #input_file='C:\\Users\mdu\\Documents\\qpr_export\\exp.xml'
-    input_file='C:\\Users\МишинДЮ\\Documents\\qpr_export\\exp.xml'
+    input_file='C:\\Users\mdu\\Documents\\qpr_export\\exp.xml'
+    #***input_file='C:\\Users\МишинДЮ\\Documents\\qpr_export\\exp.xml'
     events = ("start", "end")
     context = etree.iterparse(input_file,events = events, tag=('{www.qpr.com}ModelElement'))
     count=0
     elements_with_dot_count=0
     #elements_with_dot_count_no_Multiplicity=0
+    corrected_modelElement={}
     err_msg={}
     for action, elem in context:
         if action=="end":
+            count+=1
             modelElement=get_model_element(elem)
             try:
+                # corrected__modelElement=copy.deepcopy(modelElement)
+                # model.insert(remove_dots(corrected__modelElement))
                 model.insert(modelElement)
             except errors.InvalidDocument as err:
                 # print("ElementName:{0}; ElementID:{1}; InvalidDocument: {2}".\
                 #         format(modelElement["name"],modelElement["id"],err))
+                #print(json.dumps( modelElement))
                 elements_with_dot_count+=1
                 err_msg[modelElement["id"]]=str(err)
                 # if "Multiplicity" not in str(err):
                 #     elements_with_dot_count_no_Multiplicity+=1
                 # exit
             #print (modelElement["name"])
-            count+=1
-            if count % 1000  == 0 or count==0:
+            if count % 1000  == 0 or count==1:
                 print(count)
                 t.stop()
                 print(t.elapsed)
                 t.reset()
                 t.start()
             elem.clear()
-        while elem.getprevious() is not None:
-            del elem.getparent()[0]
-        # if count>1000:
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+        # if count>=1000:
         #     break
     del context
+    print(count)
+    t.stop()
+    print(t.elapsed)
+    time_total.stop()
+    print("Total import time:{}".format(time_total.elapsed))
     print("Total elements:{}".format(count))
     #print("Elements with dot in field name:{}".format(elements_with_dot_count))
     print("Inserted documents:{}".format(model.count()))
-    db.orders.count()
-    t.stop()
-    print(t.elapsed)
+    #db.orders.count()
     err_msg_uniq=uniq(err_msg.values())
     print("Elements with dot or $  in field name:{}".format(len(err_msg)))
     print("Unique errors :{}".format(len( err_msg_uniq)))
     for msg in err_msg_uniq:
         print(msg)
-    #print(err_msg)
-    # end_time=datetime.now()
-    # print("End processing: "+end_time.strftime("%d.%m.%Y %H:%M:%S.%f"))
-    # duration = end_time-start_time;
-    # duration_str = "%d.%d" % (duration.seconds, duration.microseconds / 1000)
-    # print("Duration: "+duration_str+"s" )
     exit(0)
 
 
--- MongoTest/test_dot_keys.py
+++ MongoTest/test_dot_keys.py
@@ -1,6 +1,4 @@
 __author__ = 'mdu'
-from pymongo import MongoClient
-from pymongo import errors
 import datetime
 try:
     from lxml import etree
@@ -11,49 +9,6 @@ from datetime import datetime
 import sys
 sys.path.append("C:\IdeaProjects\tomita_tools\MongoTest")
 from Timer import *
-
-def get_model_element_(elem):
-    modelElement=[]
-    #*****************************
-    modelElement.append(dict(parent_object=""))
-    modelElement.append(dict(revision=""))
-    modelElement.append(dict(model_revision=""))
-    modelElement.append(dict(description=""))
-    modelElement.append(dict(native_id=""))
-    modelElement.append(dict(creation_date=datetime.now()))
-    modelElement.append(dict(change_date=datetime.now()))
-    modelElement.append(dict(archive="false"))
-    modelElement.append(dict(deletede="false"))
-    #***************************************************
-
-    modelElement.append(dict(name=next(iter(elem.xpath("./@ElementName")),"")))
-    modelElement.append(dict(id=next(iter(elem.xpath("./@ElementId")),"")))
-    modelElement.append(dict(type=next(iter(elem.xpath("./@TypeID")),"")))
-    data_section_2=[]
-    for attributeElement in elem.xpath("./www.qpr.com:Attribute",namespaces={'www.qpr.com': 'www.qpr.com'}):
-        fieldName=next(iter(attributeElement.xpath("./@AttributeName")),"")
-        field={}
-        field_values_array=[]
-        valueArray=[]
-        valueArray.extend(attributeElement.xpath("./www.qpr.com:Value/text()",namespaces={'www.qpr.com': 'www.qpr.com'}))
-        if len(valueArray):
-            field_values_array.append(dict(Value=valueArray))
-        records=[]
-        for recordElement in attributeElement.xpath("./www.qpr.com:Record",namespaces={'www.qpr.com': 'www.qpr.com'}):
-            record=[]
-            for recordFieldElement in recordElement.xpath("./www.qpr.com:Field",namespaces={'www.qpr.com': 'www.qpr.com'}):
-                recordFieldName=next(iter(recordFieldElement.xpath("./@Name")),"")
-                recordFieldValue=next(iter(recordFieldElement.xpath("./@Value")),"")
-                recordField={}
-                recordField[recordFieldName]=recordFieldValue
-                record.append(recordField)
-            records.append(record)
-        if len(records):
-            field_values_array.append(dict(Record=records))
-        field[fieldName]=field_values_array
-        data_section_2.append(field)
-    modelElement.append(dict(data_section_2=data_section_2))
-    return modelElement
 
 def get_model_element(elem):
     modelElement={}
@@ -102,62 +57,123 @@ def uniq(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
+
 if __name__ == '__main__':
     t = Timer()
     t.start()
-    # start_time=datetime.now()
-    # print("Start processing: "+start_time.strftime("%d.%m.%Y %H:%M:%S.%f"))
-    client = MongoClient()
-    db=client.modelDB
-    model = db.model
 
     #input_file='C:\\IdeaProjects\\hh_api_test\\MongoTest\\exp_types_formatted_few_elements.xml'
     #***input_file='exp_types_formatted_few_elements.xml'
-    #input_file='C:\\Users\mdu\\Documents\\qpr_export\\exp.xml'
-    input_file='C:\\Users\ÌèøèíÄÞ\\Documents\\qpr_export\\exp.xml'
+    input_file='C:\\Users\mdu\\Documents\\qpr_export\\exp.xml'
+    #input_file='C:\\Users\mdu\\Documents\\qpr_export\\exp_types_formatted_few_elements_dots.xml'
+    #***input_file='C:\\Users\ÐœÐ¸ÑˆÐ¸Ð½Ð”Ð®\\Documents\\qpr_export\\exp.xml'
     events = ("start", "end")
     context = etree.iterparse(input_file,events = events, tag=('{www.qpr.com}ModelElement'))
     count=0
+    dot_key_count=0
+    key_count=0
     elements_with_dot_count=0
-    #elements_with_dot_count_no_Multiplicity=0
-    err_msg={}
+    # key_path="./descendant-or-self::Attribute/Record/Field/@Name[contains(., '.')]|" \
+    #          "./descendant-or-self::*/@AttributeName[# contains(., '.')]"
+    key_path_1="./www.qpr.com:Attribute/www.qpr.com:Record/www.qpr.com:Field[@Name[contains(., '.')]]"
+    key_path_1_c="./www.qpr.com:Attribute/www.qpr.com:Record/www.qpr.com:Field/@Name"
+    key_path_2="./www.qpr.com:Attribute[@AttributeName[contains(., '.')]]"
+    key_path_2_c="./www.qpr.com:Attribute/@AttributeName"
+    #key_path="./www.qpr.com:Attribute/www.qpr.com:Record/www.qpr.com:Field"
+    dot_keys_index=[]
+    dot_keys={}
     for action, elem in context:
         if action=="end":
-            #modelElement=get_model_element(elem)
-#*******************
-#/descendant-or-self::Attribute/Record/Field/@Name[contains(., '.')] | /descendant-or-self::*/@AttributeName[# contains(., '.')] | /descendant-or-self::*/@*[contains(., '.')]
-for attributeElement in elem.xpath("./descendant-or-self::*[@*[contains(., '.')]]",
-                                               namespaces={'www.qpr.com': 'www.qpr.com'}):
-                value=next(iter(attributeElement.xpath("./@AttributeName")),"")
-        field={}
-#****************************
-        count+=1
-            if count % 1000  == 0 or count==0:
+            count+=1
+            #print(elem.attrib["ElementName"])
+            tree = etree.ElementTree(elem)
+            key_count+=int(elem.xpath("count("+key_path_1_c+")",namespaces={'www.qpr.com': 'www.qpr.com'}))
+            for attributeElement in elem.xpath(key_path_1,namespaces={'www.qpr.com': 'www.qpr.com'}):
+                dot_element_path=str(tree.getelementpath(attributeElement))
+                dot_element_attribute=str(attributeElement.attrib["Name"])
+                dot_element= "{{www.qpr.com}}ModelElement[{}]/".format(count)+dot_element_path+"/@"+dot_element_attribute
+                dot_keys_index.append(dot_element)
+                dot_keys[dot_element]=list([dot_element_path])
+                #dot_keys[dot_element].append(str(attributeElement.attrib["Name"]))
+                dot_keys[dot_element].append(dot_element_attribute)
+                dot_key_count+=1
+            key_count+=int(elem.xpath("count("+key_path_2_c+")",namespaces={'www.qpr.com': 'www.qpr.com'}))
+            for attributeElement in elem.xpath(key_path_2,namespaces={'www.qpr.com': 'www.qpr.com'}):
+                dot_element_path=str(tree.getelementpath(attributeElement))
+                dot_element_attribute=str(attributeElement.attrib["AttributeName"])
+                dot_element= "{{www.qpr.com}}ModelElement[{}]/".format(count)+dot_element_path+"/@"+dot_element_attribute
+                dot_keys_index.append(dot_element)
+                dot_keys[dot_element]=list([dot_element_path])
+                dot_keys[dot_element].append(dot_element_attribute)
+                #dot_element=str(tree.getelementpath(attributeElement))+str(attributeElement.attrib["AttributeName"])
+                dot_key_count+=1
+            if count % 1000  == 0 or count==1:
                 print(count)
                 t.stop()
                 print(t.elapsed)
                 t.reset()
                 t.start()
             elem.clear()
-        while elem.getprevious() is not None:
-            del elem.getparent()[0]
-            # if count>1000:
-            #     break
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+        # if count>=1000:
+        #     break
     del context
-    print("Total elements:{}".format(count))
+    uniq_dot_keys=uniq(dot_keys[i][1]for i in dot_keys)
+
+    print("Total ModelElement:{}".format(count))
+    print("Total key attributes:{}".format(key_count))
+    print("Key attributes with dots:{}".format(dot_key_count))
+    print("Unique Key attributes with dots:{}".format(len(uniq(dot_keys[i][1]for i in dot_keys))))
+    test_cont=0
+    for i in dot_keys:
+        if dot_keys[i][1] in uniq_dot_keys:
+            test_cont+=1
+    print("Test = Key attributes with dots:{}".format( test_cont))
+    #exit (0)
+    context = etree.iterparse(input_file,events = events, tag=('{www.qpr.com}ModelElement'))
+    #attr_path='.//*/@*[not(name()="AttributeName" or name()="Name")]'
+    attr_path='.//@*[ not(name()="Name" or name()="AttributeName")]'
+    value_path='.//www.qpr.com:Value/text()'
+    #attr_path='.//*/@*'
+    dot_keys_in_values_count_values=0
+    dot_keys_in_values_count_attributes=0
+    cmp_count=0
+    key_count_attributes=0
+    key_count_values=0
+    #key_count=0
+    for action, elem in context:
+        if action=="end":
+            cmp_count+=1
+            key_count_attributes+=int(elem.xpath("count("+attr_path+")",namespaces={'www.qpr.com': 'www.qpr.com'}))
+            #print("key_count:{}".format(key_count))
+            for attributeElement in elem.xpath(attr_path,namespaces={'www.qpr.com': 'www.qpr.com'}):
+                #print(attributeElement)
+                if str(attributeElement) in  uniq_dot_keys:
+                    dot_keys_in_values_count_attributes+=1
+            key_count_values+=int(elem.xpath("count("+value_path+")",namespaces={'www.qpr.com': 'www.qpr.com'}))
+            for attributeElement in elem.xpath(value_path,namespaces={'www.qpr.com': 'www.qpr.com'}):
+                #print(attributeElement)
+                if str(attributeElement) in  uniq_dot_keys:
+                    dot_keys_in_values_count_values+=1
+
+            if cmp_count % 100  == 0 or count==1:
+                print(cmp_count)
+            elem.clear()
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+                # if cmp_count>=1000:
+                #     break
+    del context
+    #print("Tect =Total key attributes:{}".format(key_count))
+    print("Total key attributes:{}".format(key_count))
+    print("Key attributes with dots:{}".format(dot_key_count))
+    print("Not Key attributes:{}".format(key_count_attributes))
+    print("Value elements:{}".format(key_count_values))
+    print("Not Key attributes contains  Key attributes with dots:{}".format(dot_keys_in_values_count_attributes))
+    print("Values contains  Key attributes with dots:{}".format(dot_keys_in_values_count_values))
     t.stop()
     print(t.elapsed)
-    err_msg_uniq=uniq(err_msg.values())
-    print("Elements with dot or $  in field name:{}".format(len(err_msg)))
-    print("Unique errors :{}".format(len( err_msg_uniq)))
-    for msg in err_msg_uniq:
-        print(msg)
-    #print(err_msg)
-    # end_time=datetime.now()
-    # print("End processing: "+end_time.strftime("%d.%m.%Y %H:%M:%S.%f"))
-    # duration = end_time-start_time;
-    # duration_str = "%d.%d" % (duration.seconds, duration.microseconds / 1000)
-    # print("Duration: "+duration_str+"s" )
     exit(0)
 
 
